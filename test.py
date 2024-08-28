445a3afd3b5d0e50626a253ea7cf170712423aaa
Cheremuxa <gaukhar.2006@bk.ru>
2024-08-28 19:38:55 +0600

    vetka

---
+++ test.py
@@ -0,0 +1,14 @@
+def moi_decorator(func):
+    def wrapper(*args, **kwargs):
+        print("prosto vetka:(")
+        result = func(*args, **kwargs)
+        print("new vetka")
+        return result
+    return wrapper
+@moi_decorator
+def say_hello():
+    print("Salam")
+
+# Example usage:
+if __name__ == "__main__":
+    say_hello()
